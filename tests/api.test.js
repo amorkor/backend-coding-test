11a1be854f9cead35295c31a599780cd497455a8
Roman Korolovych <63035250+amorkor@users.noreply.github.com>
2022-02-10 16:34:04 +0200

    Tests(API): Add test for GET /rides pagination

--- tests/api.test.js
+++ tests/api.test.js
@@ -32,9 +32,14 @@ describe('API tests', () => {
         });
     });
 
-    describe('Testing /ride endpoint', () => {
+    describe('Testing /rides endpoint', () => {
         describe('Creating ride', () => {
-            const { ride, validationError } = testData;
+            const { ride, rideResponse, validationError } = testData;
+
+            after((done) => {
+                db.run('DELETE FROM Rides');
+                done();
+            });
 
             it('should create new ride', (done) => {
                 request(app)
@@ -44,6 +49,23 @@ describe('API tests', () => {
                     .expect('Content-Type', /json/)
                     .expect(200, done);
             });
+
+            it('should return the same created ride', (done) => {
+                request(app)
+                    .get(`/rides/${rideResponse.rideID}`)
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        assert.equal(
+                            res.body[0].rideID,
+                            rideResponse.rideID,
+                            'recieved correct ride',
+                        );
+
+                        done();
+                    });
+            });
+
             describe('Try to insert invalid data', () => {
                 it('should not pass empty rider name', (done) => {
                     request(app)
@@ -178,43 +200,153 @@ describe('API tests', () => {
         });
 
         describe('Fetching rides', () => {
-            const { rideResponse, validationError } = testData;
+            const { ride, validationError } = testData;
 
-            it('should return the same created ride', (done) => {
-                request(app)
-                    .get(`/rides/${rideResponse.rideID}`)
-                    .expect('Content-Type', /json/)
-                    .expect(200)
-                    .end((err, res) => {
-                        assert.equal(
-                            res.body[0].rideID,
-                            rideResponse.rideID,
-                            'recieved correct ride',
-                        );
-
-                        done();
-                    });
+            before((done) => {
+                Promise.all([
+                    request(app)
+                        .post('/rides')
+                        .set('Content-Type', 'application/json')
+                        .send(ride)
+                        .expect(200),
+                    request(app)
+                        .post('/rides')
+                        .set('Content-Type', 'application/json')
+                        .send(ride)
+                        .expect(200),
+                    request(app)
+                        .post('/rides')
+                        .set('Content-Type', 'application/json')
+                        .send(ride)
+                        .expect(200),
+                ]).then(() => done());
             });
 
-            it('should return array of all rides', (done) => {
-                request(app)
-                    .get('/rides')
-                    .expect('Content-Type', /json/)
-                    .expect(200)
-                    .end((err, res) => {
-                        assert.typeOf(
-                            res.body,
-                            'array',
-                            'recieved array of rides',
-                        );
-
-                        done();
-                    });
+            after((done) => {
+                db.run('DELETE FROM Rides');
+                done();
             });
+
+            describe('GET /rides', () => {
+                it('without queries should return page with all rides', (done) => {
+                    request(app)
+                        .get('/rides')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end((err, res) => {
+                            assert.typeOf(
+                                res.body,
+                                'object',
+                                'recieved object with rides',
+                            );
+                            assert.property(
+                                res.body,
+                                'pagedRows',
+                                'recieved object has fetched rides',
+                            );
+                            assert.equal(
+                                res.body.pagedRows.length,
+                                3,
+                                'recieved object has all rides',
+                            );
+
+                            done();
+                        });
+                });
+
+                it('with invalid limit should return page with all rides', (done) => {
+                    request(app)
+                        .get('/rides?limit=dummy')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end((err, res) => {
+                            assert.typeOf(
+                                res.body,
+                                'object',
+                                'recieved object with rides',
+                            );
+                            assert.property(
+                                res.body,
+                                'pagedRows',
+                                'recieved object has fetched rides',
+                            );
+                            assert.equal(
+                                res.body.pagedRows.length,
+                                3,
+                                'recieved object has all rides',
+                            );
+
+                            done();
+                        });
+                });
+
+                it('with page < 1 should return first page', (done) => {
+                    request(app)
+                        .get('/rides?limit=1&page=-1')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end((err, res) => {
+                            assert.typeOf(
+                                res.body,
+                                'object',
+                                'recieved object with rides',
+                            );
+                            assert.property(
+                                res.body,
+                                'pagedRows',
+                                'recieved object has fetched rides',
+                            );
+                            assert.equal(
+                                res.body.pagedRows.length,
+                                1,
+                                'one ride per page',
+                            );
+                            assert.equal(
+                                res.body.currentPage,
+                                1,
+                                'recieved first page',
+                            );
+
+                            done();
+                        });
+                });
+
+                it('with page > total pages should last page', (done) => {
+                    request(app)
+                        .get('/rides?limit=1&page=10000000')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end((err, res) => {
+                            assert.typeOf(
+                                res.body,
+                                'object',
+                                'recieved object with rides',
+                            );
+                            assert.property(
+                                res.body,
+                                'pagedRows',
+                                'recieved object has fetched rides',
+                            );
+                            assert.equal(
+                                res.body.pagedRows.length,
+                                1,
+                                'one ride per page',
+                            );
+                            assert.equal(
+                                res.body.currentPage,
+                                3,
+                                'recieved last page',
+                            );
+
+                            done();
+                        });
+                });
+            });
+
 
             it('should return not found message', (done) => {
                 request(app)
-                    .get('/rides/5')
+                    .get('/rides/-1')
                     .expect('Content-Type', /json/)
                     .expect(200)
                     .end((err, res) => {
@@ -233,6 +365,5 @@ describe('API tests', () => {
                     });
             });
         });
-
     });
 });
