e08d1c48e0a15982f40d48eaca550f1696d26fe7
Roman Korolovych <63035250+amorkor@users.noreply.github.com>
2022-02-11 16:06:59 +0200

    Ref(app): move app building & initialization to src/app.js

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 'use strict';
-
-const express = require('express');
-const app = express(); // eslint-disable-line no-unused-vars
-const port = 8010;
-
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json(); // eslint-disable-line no-unused-vars
-
-const { connect } = require('./src/database');
 const logger = require('./src/util/logger');
+const startApp = require('./src/app');
 
-const buildSchemas = require('./src/database/schemas');
-const RideRouter = require('./src/routes/RideRouter');
-
-(async () => {
-    const db = await connect(':memory:');
-    const dbWithCallback = db.getDatabaseInstance();
-
-    await buildSchemas(db);
-
-    const app = require('./src/app')(dbWithCallback);
-    const rideRouter = RideRouter(db, logger);
-    app.use(
-        '/rides',
-        jsonParser,
-        rideRouter
-    );
-
-    app.listen(port, (err) => {
-        if (err) {
-            logger.error({
-                message: err.message,
-                code: err.code,
-            });
-        }
-
-        logger.info(`App started and listening on port ${port}`);
-    });
-
-})();
+try {
+    (async () => {
+        startApp();
+    })();
+} catch (err) {
+    logger.error(err);
+}
--- src/app.js
+++ src/app.js
@@ -2,11 +2,26 @@
 
 const express = require('express');
 const app = express();
+const RideRouter = require('./routes/RideRouter');
+
 const redoc = require('redoc-express');
 
-module.exports = () => {
-    app.get('/health', (req, res) => res.send('Healthy'));
+const { connect } = require('./database');
+const buildSchemas = require('./database/schemas');
+const logger = require('./util/logger');
 
+const PORT = 8010;
+
+const startApp = async () => {
+    const database = await connect(':memory:');
+    const rideRouter = RideRouter(database, logger);
+
+    await buildSchemas(database);
+
+    app.use(express.json());
+
+    app.use('/rides', rideRouter);
+    app.get('/health', (req, res) => res.send('Healthy'));
     app.get(
         '/docs',
         redoc({
@@ -14,10 +29,20 @@ module.exports = () => {
             specUrl: '/docs/swagger.yaml',
         })
     );
-
     app.get('/docs/swagger.yaml', (req, res) => {
         res.sendFile('swagger.yaml', { root: '.' });
     });
 
-    return app;
+    app.listen(PORT, (err) => {
+        if (err) {
+            logger.error({
+                message: err.message,
+                code: err.code,
+            });
+        }
+
+        logger.info(`App started and listening on port ${PORT}`);
+    });
 };
+
+module.exports = startApp;
